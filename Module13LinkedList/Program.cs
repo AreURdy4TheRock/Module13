d3a42f73a9da30a2cffaa3ba60d87aad68d75476
AreURdy4TheRock <dorkwingdock@gmail.com>
2023-05-04 21:39:50 +0300

    Добавьте файлы проекта.

---
+++ Module13LinkedList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Module13LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "f:\\cdev_Text.txt";
+            var strings = new LinkedList<string>();
+            // читаем весь файл с рабочего стола в строку текста
+            string text = File.ReadAllText(path);
+            // Сохраняем символы-разделители в массив
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+            // убираем знааки препинания
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            // разбиваем нашу строку текста, используя ранее перечисленные символы-разделители
+            var words = noPunctuationText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            // запускаем новый таймер
+            var stopWatch = Stopwatch.StartNew();
+
+            foreach (var word in words)
+            {
+                strings.AddLast(word);
+            }
+            // смотрим, сколько операция заняла, в миллисекундах
+            Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
+
+        }
+    }
+}
